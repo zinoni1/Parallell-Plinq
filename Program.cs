e527e0bb9fc3f533dce29ff352fe8012386aae89
Zenon <zenropru@gmail.com>
2024-02-13 23:10:47 +0100

    Paralel i plinq acabat

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Parallel_Plinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("people.json");
+            List<Usuari> _users = JsonConvert.DeserializeObject<List<Usuari>>(json);
+
+            ListView lvJSON = new ListView();
+
+            foreach (Usuari _user in _users)
+            {
+                ListViewItem _list = new ListViewItem(_user.Name);
+                _list.SubItems.Add(_user.Dni);
+                _list.SubItems.Add("true o false");
+                lvJSON.Items.Add(_list);
+            }
+            ComprovaSeqüencial(_users);
+
+            ComprovaParallel(_users);
+
+            Console.ReadLine();
+        }
+
+        static void ComprovaSeqüencial(List<Usuari> usuaris)
+        {
+            var startTime = DateTime.Now;
+
+            foreach (Usuari usuari in usuaris)
+            {
+                bool dniCorrecte = usuari.ComprovaDNI();
+                bool nomCorrecte = usuari.ComprovaNom();
+                bool mailCorrecte = usuari.ComprovaMail();
+
+                Console.WriteLine($"Seqüencial - Nom: {usuari.Name}, DNI Correcte: {dniCorrecte}, Nombre Correcte: {nomCorrecte}, Mail Correcte: {mailCorrecte}");
+            }
+            Console.WriteLine($"Temps Seqüencial: {(DateTime.Now - startTime).TotalMilliseconds} ms");
+        }
+        
+        static void ComprovaParallel(List<Usuari> usuaris)
+        {
+            var startTime = DateTime.Now;
+
+            Parallel.ForEach(usuaris, usuari =>
+            {
+                bool dniCorrecte = usuari.ComprovaDNI();
+                bool nomCorrecte = usuari.ComprovaNom();
+                bool mailCorrecte = usuari.ComprovaMail();
+
+                Console.WriteLine($"Paral·lel - Nom: {usuari.Name}, DNI Correcte: {dniCorrecte}, Nombre Correcte: {nomCorrecte}, Mail Correcte: {mailCorrecte}");
+            });
+
+            Console.WriteLine($"Temps Paral·lel: {(DateTime.Now - startTime).TotalMilliseconds} ms");
+        }
+    }
+}
---
+++ Usuari.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+
+namespace Parallel_Plinq
+{
+    public class Usuari
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public string Company { get; set; }
+        public string Country { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }
+
+        public Usuari(string name, string dni, string mail)
+        {
+            Name = name;
+            Dni = dni;
+            Email = mail;
+
+           
+        }
+        public bool ComprovaDNI()
+        {
+            if (string.IsNullOrEmpty(Dni) || Dni.Length != 9)
+                return false;
+
+            string digits = Dni.Substring(0, 8);
+            string lletra = Dni.Substring(8, 1);
+
+            int n;
+            if (!int.TryParse(digits, out n))
+                return false;
+
+            string lletres = "TRWAGMYFPDXBNJZSQVHLCKE";
+            int index = n % 23;
+
+            return lletra == lletres.Substring(index, 1);
+        }
+
+        public bool ComprovaNom()
+        {
+            return !string.IsNullOrEmpty(Name);
+        }
+
+        public bool ComprovaMail()
+        {
+            if (string.IsNullOrEmpty(Email) || !Email.Contains("@"))
+                return false;
+
+            if (Email.IndexOf('@') != Email.LastIndexOf('@') || Email.IndexOf('@') == 0 || Email.IndexOf('@') == Email.Length - 1)
+                return false;
+
+            return true;
+        }
+    }
+}
